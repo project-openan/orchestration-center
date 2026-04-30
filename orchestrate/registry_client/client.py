e2c1f9800bfce1493f3a1ce460790e9b2af1f122
qxz250413467 <quanxinzhao2@h-partners.com>
2026-04-30 14:59:42 +0800

    feat: feat: API change adaptation

--- orchestrate/registry_client/client.py
+++ orchestrate/registry_client/client.py
@@ -114,7 +114,7 @@ class AgentRegistryClient:
         if provider:
             parms['provider'] = provider
         resp = self._request('GET', f'/rest/v1/registry-center/agent-cards', params=parms)
-        return resp.json()
+        return resp.json().get("agentCards", [])
 
     def search_by_task(self, task: str) -> List[dict]:
         """
