7389e21d917c30feefa8b4b7cbbe12d3a555ce55
ZhouJie6 <ivo.zhou@huawei.com>
2026-06-10 08:58:57 +0800

    chore: add SPDX-License-Identifier to all source file headers

--- common/a2at_config.py
+++ common/a2at_config.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2026 Huawei Technologies Co., Ltd.
 # All Rights Reserved.
 #
+# SPDX-License-Identifier: Apache-2.0
+#
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
 #    a copy of the License at
@@ -69,6 +71,8 @@ def generate_env_from_llm_config(
 
     env_content = f"""# Copyright (c) 2026 Huawei Technologies Co., Ltd.
 # All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
